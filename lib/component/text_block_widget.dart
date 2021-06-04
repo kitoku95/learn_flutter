2db5de1bf1715de325b428ef8db4a25267cfef12
kitoku95 <caocongduc95@gmail.com>
2021-06-04 10:43:00 +0900

    begin learning

---
+++ lib/component/text_block_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextBlockWidget extends StatelessWidget{
+  
+  final String text;
+  final bool isAsk;
+
+  const TextBlockWidget({Key key, this.text, this.isAsk}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(isAsk ? "Q: " + this.text : "A: " + this.text),
+    );
+  }
+  
+}
